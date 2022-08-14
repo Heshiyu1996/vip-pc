32282dca0af59c126a6725a192bcbc11d0c01152
heshiyu <heshiyu@corp.netease.com>
2022-08-14 11:45:51 +0800

    feat: 目录框架

--- config/routes.ts
+++ config/routes.ts
@@ -1,4 +1,107 @@
 ﻿export default [
+  // 首页
+  {
+    path: '/home',
+    name: 'home',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  // 会员卡管理
+  {
+    path: '/vip',
+    name: 'vip',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/vip/list',
+        name: 'list',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/vip/config',
+        name: 'config',
+        icon: 'smile',
+        component: './TableList',
+      },
+    ],
+  },
+  // 支付管理
+  {
+    path: '/payment',
+    name: 'payment',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/payment/recharge',
+        name: 'recharge',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/payment/recharge-list',
+        name: 'recharge-list',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/payment/consumption-list',
+        name: 'consumption-list',
+        icon: 'smile',
+        component: './TableList',
+      },
+    ],
+  },
+  // 订房管理
+  {
+    path: '/room',
+    name: 'room',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/room/arrange',
+        name: 'arrange',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/room/config',
+        name: 'config',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/room/order',
+        name: 'order',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/room/refund',
+        name: 'refund',
+        icon: 'smile',
+        component: './TableList',
+      },
+    ],
+  },
+  // 店铺管理
+  {
+    path: '/store',
+    name: 'store',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/store/config',
+        name: 'config',
+        icon: 'smile',
+        component: './TableList',
+      },
+    ],
+  },
   {
     path: '/user',
     layout: false,
@@ -12,35 +115,6 @@
         component: './404',
       },
     ],
-  },
-  {
-    path: '/welcome',
-    name: 'welcome',
-    icon: 'smile',
-    component: './Welcome',
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    routes: [
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        icon: 'smile',
-        component: './Welcome',
-      },
-      {
-        component: './404',
-      },
-    ],
-  },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
   },
   {
     path: '/',
