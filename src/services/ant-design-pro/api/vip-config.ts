92f7766862019493ccdefec448c399dfa2c26925
heshiyu <450979309@qq.com>
2022-08-15 21:46:29 +0800

    feat: api vip-config

--- src/services/ant-design-pro/api/vip-config.ts
+++ src/services/ant-design-pro/api/vip-config.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 import { request } from 'umi';
 
-/** 查询会员卡配置列表 GET /api/vip/config/list */
+/** 查询会员卡配置列表 GET /api/vip/level/list */
 export async function getVipConfigList(
   params: {
     // query
@@ -13,7 +13,7 @@ export async function getVipConfigList(
   },
   options?: { [key: string]: any },
 ) {
-  return request<API.RuleList>('/api/vip/config/list', {
+  return request<API.RuleList>('/api/vip/level/list', {
     method: 'GET',
     params: {
       ...params,
@@ -22,27 +22,27 @@ export async function getVipConfigList(
   });
 }
 
-/** 编辑会员卡配置 PUT /api/vip/config/{id} */
+/** 编辑会员卡配置 PUT /api/vip/level/{id} */
 export async function editVipConfig(params?: { [key: string]: any }, options?: { [key: string]: any }) {
-  return request<API.RuleListItem>(`/api/vip/config/${params.id}`, {
+  return request<API.RuleListItem>(`/api/vip/level/${params.id}`, {
     method: 'PUT',
     data: params,
     ...(options || {}),
   });
 }
 
-/** 新增会员卡配置 POST /api/vip/config/add */
+/** 新增会员卡配置 POST /api/vip/level/add */
 export async function addVipConfig(params?: { [key: string]: any }, options?: { [key: string]: any }) {
-  return request<API.RuleListItem>('/api/vip/config/add', {
+  return request<API.RuleListItem>('/api/vip/level/add', {
     method: 'POST',
     data: params,
     ...(options || {}),
   });
 }
 
-/** 删除会员卡 DELETE /api/vip/config/{id} */
+/** 删除会员卡 DELETE /api/vip/level/{id} */
 export async function removeVipConfig(params?: { [key: string]: any }, options?: { [key: string]: any }) {
-  return request<Record<string, any>>(`/api/vip/config/${params.id}`, {
+  return request<Record<string, any>>(`/api/vip/level/${params.id}`, {
     method: 'DELETE',
     data: params,
     ...(options || {}),
